MAJOR BUG FIXES IN GRADIENT - shit was completly wrong lmao now its good

OptimizationMethods.py
@@ -23,42 +23,55 @@ def heuristic(n, b, N, C, S):
     return deltaB
 
 
-def Sn_function(G, deltaR):
+def Sn_function(G, deltaR, p_p):
     N = G.number_of_nodes()
-    c = [0 for i in range(G.number_of_nodes())]
-    d = [0 for i in range(G.number_of_nodes())]
-    sigma = [0 for i in range(G.number_of_nodes())]
-    xN = sym.symbols('x0:%d'%(G.number_of_nodes()))  # N-tuple of vaccines deployed
+    c = [0 for i in range(N)]
+    d = [0 for i in range(N)]
+    sigma = [0 for i in range(N)]
+    xN = sym.symbols('x0:%d'%(N))  # N-tuple of vaccines deployed
     for i in G.nodes:
         node = G.nodes[i]['superUrn']
-        c[i] = node.super_R + sum([np.dot(j.delta[1:], j.Zn[1:]) for j in node.Ni_list]) + sum([deltaR*j.Zn[0]
-                                                                                                for j in node.Ni_list])
-        d[i] = node.super_B + node.super_R + sum(node.delta[1:]) + sum([j.delta[0] * j.Zn[0] for j in node.Ni_list])
+
+        # sum of all the red balls in a given super urn. note the use of p_p (pre_post). slicing of delta
+        # depending on whether the optimization is pre-draw or post-draw (0 for pre, 1 for post)
+        # so time indices of delta and Zn (which may be 1 time step ahead) are lined up
+        s_delta = 1-p_p
+        e_delta = len(node.delta)-p_p
+        c[i] = node.super_R + sum([np.dot(j.delta[s_delta:e_delta], j.Zn[1:]) for j in node.Ni_list]) + \
+               sum([deltaR*j.Zn[0] for j in node.Ni_list])
+
+        d[i] = node.super_B + node.super_R + sum(node.delta[s_delta:e_delta]) + \
+               sum([deltaR * j.Zn[0] for j in node.Ni_list])
+
         sigma[i] = sum([xN[j.key] * (1 - j.Zn[0]) for j in node.Ni_list])
-    return [xN, sum([c[i]/(d[i] + sigma[i]) for i in range(len(c))])/N]
+
+    return [xN, sum([c[i]/(d[i] + sigma[i]) for i in range(N)])/N]
 
 
-def gradient(G, T, B, deltaR):
+def gradient(G, T, p_p, B, deltaR):
     N = G.number_of_nodes()
     y = [[] for i in range(T+1)]
-    y[0] = [0 for i in range(N)]
-    y[0][0] = B
+    y[0] = [1 for i in range(N)]
     y_bar = y[:][:]
-    [xN, f_n] = Sn_function(G, deltaR)
+    [xN, f_n] = Sn_function(G, deltaR, p_p)
     f_partials = sym.derive_by_array(f_n, xN)
     if f_partials == N * [0]: return evenDistribution(N, B)  # if all partials are 0, do uniform dist of vaccines
+
+    # f_part_def = [f_partials[i].subs([(xN[j], 1) for j in range(N)]) for i in range(N)]
+    # index = np.argmin(f_part_def)
+
     for k in range(T):
         #
-        f_part_def = [f_partials[i].subs([(xN[j], 1) for j in range(N)]) for i in range(N)]
+        f_part_def = [f_partials[i].subs([(xN[j], y[k][j]) for j in range(N)]) for i in range(N)]
         index = np.argmin(f_part_def)
         #
         y_bar[k] = [0 for i in range(N)]
-        y[k][index] = B
+        y_bar[k][index] = B
         #
         alpha_list = [i/100 for i in range(1, 101)]
         f_def = [f_n.subs([(xN[j], y[k][j] + alpha_list[i] * (y_bar[k][j]-y[k][j])) for j in range(N)])
                  for i in range(100)]
-        alpha = np.argmin(f_def)
+        alpha = alpha_list[np.argmin(f_def)]
         #
         y[k+1] = [y[k][i] + alpha * (y_bar[k][i] - y[k][i]) for i in range(N)]
 