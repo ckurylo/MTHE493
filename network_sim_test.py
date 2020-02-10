280d4ee444899ac439e99bad02e1c316486afbe8
natalieranta <37555625+natalieranta@users.noreply.github.com>
2020-02-10 12:01:48 -0500

    Add files via upload
    added percolation centrality as an option

--- network_sim_test.py
+++ network_sim_test.py
@@ -7,7 +7,6 @@ import csv
 def importG(graphName):
     return pd.read_csv(graphName, header=None).values.tolist()
 
-
 def generateBallProportions(delta, n):
     maxdelta = max(delta)
     B_bd = [maxdelta * 10, maxdelta * 11]
@@ -29,7 +28,6 @@ def get_balls(ballName):
         BR[1] = int(BR[1])
         balls.append(BR)
     return balls
-
 
 def polya_sim_test(adjFile, ballFile, delta, max_n, num_sim, m_mem, num_nodes, outputFile, opt_method, tenacity):
 
@@ -58,20 +56,19 @@ def polya_sim_test(adjFile, ballFile, delta, max_n, num_sim, m_mem, num_nodes, o
 
 ###############################
 # PARAMETER INPUT
-predraw_factor = 1
-max_n = predraw_factor * 200
-m_mem = predraw_factor * 10
-num_sim = 50
+max_n = 10
+m_mem = 2
+num_sim = 10
 # adjFile = '100_node_adj_2.csv'
 # outputFile = 'weight_demo_metrics.csv'
 # ballFile = 'ball_proportions_100_nodes.csv'
 
-adjFile = '6node_center.csv'
-outputFile = 'post_grad_6N_center_50sim_ini2.csv'
+adjFile = '6node.csv'
+outputFile = 'post_grad_6N_10sim.csv'
 ballFile = '6node_proportions.csv'
 ########
-budget = 50 / predraw_factor
-deltaR = 5 / predraw_factor
+budget = 20
+deltaR = 2
 tenacity = 1  # weight of node's own Urn in Super Urn
 adj_matrix = importG(adjFile)
 # lmax = max(numpy.linalg.eig(adj_matrix)[0])
@@ -79,7 +76,7 @@ adj_matrix = importG(adjFile)
 # deltaB = int(lmax)
 # deltaR = deltaB*2
 
-opt_method = [4, 3, 1]
+opt_method = [3, 4]
 # opt_method: [1] for uniform vaccine deployment, [2] for random
     # [3, i] for heuristic with i = 1 for deg cent, 2 for close cent, 3 for bet cent
     # [4, T, k] for gradient descent, T the number of iterations of the algo for each time step
